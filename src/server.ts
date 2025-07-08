937553aea4cbef3dcb7a5818321882b73df2ebe2
albqvictor1508 <albq.victor@gmail.com>
2025-07-08 11:28:44 -0300

    chore: initial commit 2

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
+import { env } from "./common/env.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.get("/health", () => {
+app.get("/health", (): string => {
   return "OK";
 })
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: Number(env.PORT) }).then((): void => {
   console.log("HTTP Server running!");
 })
