a83d1beb567387cba2ff56d865d4d2cdbe002f85
albqvictor1508 <albq.victor@gmail.com>
2025-07-11 08:24:11 -0300

    chore: changed from native node to tsx and tsup

--- src/db/client.ts
+++ src/db/client.ts
@@ -3,9 +3,9 @@ import { drizzle } from "drizzle-orm/postgres-js"
 import { schema } from "./schema/index.ts"
 import { env } from "../common/env.ts";
 
-const pg = postgres(env.DATABASE_URL);
+export const sql = postgres(env.DATABASE_URL);
 
-export const db = drizzle(pg, {
+export const db = drizzle(sql, {
   schema,
   casing: "snake_case"
 });
